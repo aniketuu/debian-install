c71f1448039082009f46e3ee53b1c54a8af80fb9
aniketuu <58410742+aniketuu@users.noreply.github.com>
2022-11-26 21:37:12 +0530

    Create nerdfonts.sh

---
+++ nerdfonts.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+sudo mkdir -p ~/.local/share/fonts
+
+cd /tmp
+fonts=( 
+"FiraCode" 
+"Go-Mono" 
+"Hack" 
+"Inconsolata" 
+"Iosevka" 
+"JetBrainsMono" 
+"Mononoki" 
+"RobotoMono" 
+"SourceCodePro" 
+"UbuntuMono"
+)
+
+for font in ${fonts[@]}
+do
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$font.zip
+    sudo unzip $font.zip -d /home/$SUDO_USER/.local/share/fonts/$font/
+    sudo chmod 644 /home/$SUDO_USER/.local/share/fonts/$font/*
+    rm $font.zip
+done
+fc-cache
