68ddfd8d4ac0d1809828dba121763fdfcd56c446
aniketuu <58410742+aniketuu@users.noreply.github.com>
2022-11-26 21:42:27 +0530

    Update nerdfonts.sh

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -19,8 +19,8 @@ fonts=(
 for font in ${fonts[@]}
 do
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$font.zip
-    sudo unzip $font.zip -d /home/$SUDO_USER/.local/share/fonts/$font/
-    sudo chmod 644 /home/$SUDO_USER/.local/share/fonts/$font/*
+    sudo unzip $font.zip -d /home/$USER/.local/share/fonts/$font/
+    sudo chmod 644 /home/$USER/.local/share/fonts/$font/*
     rm $font.zip
 done
 fc-cache
