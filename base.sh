83990d9be809264d6b8d130753e367c981e105c2
aniketuu <58410742+aniketuu@users.noreply.github.com>
2022-11-26 16:57:33 +0530

    Update base.sh

--- base.sh
+++ base.sh
@@ -3,3 +3,16 @@
 printf "\033c" #reset the terminal
 echo "Debian base install script"
 
+if [[ $EUID -ne 0 ]]; then
+ echo "Please run as super user"
+ exit 1
+fi
+
+# update to testing
+echo "deb http://deb.debian.org/debian/ testing main non-free contrib" > /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ testing main non-free contrib" >> /etc/apt/sources.list
+apt update
+apt full-upgrade
+
+# Install nala
+apt install nala -y
