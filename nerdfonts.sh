d244be900d3d98fc886a3a9828a0659080ccbe41
aniketuu <58410742+aniketuu@users.noreply.github.com>
2022-11-26 21:51:01 +0530

    Update nerdfonts.sh

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -13,8 +13,8 @@ fonts=("FiraCode" "Go-Mono" "Hack" "Inconsolata" "Iosevka" "JetBrainsMono" "Mono
 for font in ${fonts[@]}
 do
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$font.zip
-    unzip $font.zip -d /home/$USER/.local/share/fonts/$font/
-    chmod 644 /home/$USER/.local/share/fonts/$font/*
+    unzip $font.zip -d /home/$SUDO_USER/.local/share/fonts/$font/
+    chmod 644 /home/$SUDO_USER/.local/share/fonts/$font/*
     rm $font.zip
 done
 fc-cache
